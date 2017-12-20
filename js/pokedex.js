26bc9be77f219fe945f0d85c3af482b9a1658706
Thiago Vilasboas <vilasboas@outlook.com.br>
2017-12-19 22:40:23 -0200

    Add files via upload

---
+++ js/pokedex.js
@@ -0,0 +1,67 @@
+var app = angular.module('myApp', []);
+ app.controller('myCtrl', function($scope, $http) {
+    $scope.allPokemons;
+    $http.get('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0').
+        then(function(response) {
+        	// console.log(response)
+            // $scope.greeting = response.data;
+            $scope.allPokemons = response.data;
+            //console.log($scope.allPokemons)
+        });
+
+    $scope.count = 0;
+
+    $scope.paginar = function() {
+    	$scope.count = $scope.count + 20;
+    	$http.get('https://pokeapi.co/api/v2/pokemon/?limit=20&offset='+$scope.count).
+        then(function(response) {
+            $scope.allPokemons = response.data;
+            //console.log($scope.allPokemons)
+        });
+    };
+	
+	$scope.getPokemonDetails = function(result) {
+        result.details = $http.get(result.url).
+            then(function(response) {
+                // console.log(response)
+            $scope.pokemonDetail = response.data;
+                return response.data;
+            });
+            console.log(result.details);
+     };
+
+    $scope.select= function(item) {
+        $scope.selected = item; 
+    };
+    $scope.isActive = function(item) {
+           return $scope.selected === item;
+    };
+
+
+    $http.get('https://pokeapi.co/api/v2/item/?limit=20&offset=0').
+        then(function(response) {
+            // console.log(response)
+            // $scope.greeting = response.data;
+            $scope.allItems = response.data;
+            //console.log($scope.allItems)
+        });
+    
+    $scope.paginarItem = function() {
+        $scope.count = $scope.count + 20;
+        $http.get('https://pokeapi.co/api/v2/item/?limit=20&offset='+$scope.count).
+        then(function(response) {
+            $scope.allItems = response.data;
+            console.log($scope.allItems)
+        });
+    };
+
+    $scope.getItemDetails = function(result) {
+        result.details = $http.get(result.url).
+            then(function(response) {
+                // console.log(response)
+            $scope.itemDetail = response.data;
+                return response.data;
+            });
+            //console.log(result.details);
+     };
+});
